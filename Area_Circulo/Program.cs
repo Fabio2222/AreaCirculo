3287730119f65a15592322f2bb87ae33bf12d3c1
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-23 15:51:53 -0300

    Adicionar arquivos de projeto.

---
+++ Area_Circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Area_Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, pi = 3.1416, area;
+            Console.WriteLine("Qual o valor do raio?");
+            raio = double.Parse(Console.ReadLine());
+            area = pi * raio * raio;
+            Console.WriteLine("A area do circulo e : {0}", area);
+        }
+    }
+}
